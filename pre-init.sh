0a4e0f8cb340d56fec61d5788fd9b0ad6383d1b0
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-06-06 12:46:04 +0200

    - directory to store htpasswd credentials

--- pre-init.sh
+++ pre-init.sh
@@ -2,5 +2,6 @@
 sudo mkdir -p /docker_volumes/letsencrypt/etc/letsencrypt/
 sudo mkdir -p /docker_volumes/docker_registry/
 sudo mkdir -p /docker_volumes/dhparam/
+sudo mkdir -p /docker_volumes/htpasswd/auth/
 sudo mkdir -p website-content
 sudo openssl dhparam -out /docker_volumes/dhparam/hub.cloudiolab.com-dhparam-4096.pem 4096
